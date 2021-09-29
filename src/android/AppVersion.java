68ba25db230955879c9e631b5e678332dd10d7cf
nosTa1337 <phhebe@gmail.com>
2021-09-29 18:13:28 +0200

    Update AppVersion.java

--- src/android/AppVersion.java
+++ src/android/AppVersion.java
@@ -32,7 +32,19 @@ public class AppVersion extends CordovaPlugin {
       }
       if (action.equals("getVersionCode")) {
         PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        PackageInfo packageInfo = packageManager.getPackageInfo(
         callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionCode);
+      return true;
+      }
+      if (action.equals("getAppSignature")) {
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), PackageManager.GET_SIGNATURES));
+        
+        Signature[] signatures = packageInfo.signatures;
+        if (signatures.length > 0) {
+            Signature signature = signatures[0];
+            return getSHA1(signature.toByteArray());
+        }
       return true;
       }
       return false;
@@ -41,5 +53,32 @@ public class AppVersion extends CordovaPlugin {
       return true;
     }
   }
+          
+  public static String getSHA1(byte[] sig) {
+      MessageDigest digest = null;
+      try {
+          digest = MessageDigest.getInstance("SHA1", "BC");
+      } catch (NoSuchAlgorithmException e) {
+          e.printStackTrace();
+      } catch (NoSuchProviderException e) {
+          e.printStackTrace();
+      }
+      digest.update(sig);
+      byte[] hashtext = digest.digest();
+      return bytesToHex(hashtext);
+  }
 
+  //util method to convert byte array to hex string
+  public static String bytesToHex(byte[] bytes) {
+      final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+              '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+      char[] hexChars = new char[bytes.length * 2];
+      int v;
+      for (int j = 0; j < bytes.length; j++) {
+          v = bytes[j] & 0xFF;
+          hexChars[j * 2] = hexArray[v >>> 4];
+          hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+      }
+      return new String(hexChars);
+  }   
 }
